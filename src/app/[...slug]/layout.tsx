b9af605fdf4494e0121eec1c4874470672bd23fa
동영 김 <sonic2001@nate.com>
2023-09-17 22:38:53 +0900

    design main page

--- src/app/[...slug]/layout.tsx
+++
@@ -1,6 +0,0 @@
-import Children from "@/components/Children";
-import styles from "./style.module.scss";
-
-export default function Layout({ children }: Children) {
-	return <div className={styles.layout}>{children}</div>;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,75 +1,26 @@
+import { allPosts } from "@/contentlayer/generated";
 import { Metadata } from "next";
+import styles from "./style.module.scss";
+import PostBox from "@/components/layout/postbox";
 
 export const metadata: Metadata = {
 	title: "temple2001.github.io",
 };
 
 export default function Page() {
+	const posts = allPosts;
+	const orderedPosts = posts.sort(
+		(a, b) => +new Date(a.createdAt) - +new Date(b.createdAt)
+	);
+
 	return (
-		<>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-			<p>Main page</p>
-		</>
+		<div className={styles.category}>
+			<div className={styles.title}>메인 페이지</div>
+			<div className={styles.flexBox}>
+				{orderedPosts.map((post) => {
+					return <PostBox post={post} />;
+				})}
+			</div>
+		</div>
 	);
 }

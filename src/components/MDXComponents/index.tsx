c35e868f24abd63eb77987c6b1fa6361be8690d3
동영 김 <sonic2001@nate.com>
2023-09-25 18:03:56 +0900

    optimize navigation

--- src/components/MDXComponents/index.tsx
+++ src/components/MDXComponents/index.tsx
@@ -4,16 +4,29 @@ import Image from "../layout/image";
 
 export const mdxComponents: MDXComponents = {
 	h1: ({ children }) => (
-		<h1 className={`${styles.heading} ${styles.heading1}`}>{children}</h1>
+		<>
+			<h1 className={`${styles.heading} ${styles.heading1}`}>{children}</h1>
+			<div className={styles.border} />
+		</>
 	),
 	h2: ({ children }) => (
-		<h2 className={`${styles.heading} ${styles.heading2}`}>{children}</h2>
+		<>
+			<h2 className={`${styles.heading} ${styles.heading2}`}>{children}</h2>
+			<div className={styles.border} />
+		</>
 	),
 	h3: ({ children }) => (
-		<h3 className={`${styles.heading} ${styles.heading3}`}>{children}</h3>
+		<>
+			<h3 className={`${styles.heading} ${styles.heading3}`}>{children}</h3>
+			<div className={styles.border} />
+		</>
 	),
 	h4: ({ children }) => (
-		<h4 className={`${styles.heading} ${styles.heading4}`}>{children}</h4>
+		<>
+			<h4 className={`${styles.heading} ${styles.heading4}`}>{children}</h4>
+			<div className={styles.border} />
+		</>
 	),
+	li: ({ children }) => <li>{children}</li>,
 	Image,
 };
--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import styles from "./style.module.scss";
 import { metadata } from "@/lib/constants";
+import TabKey from "../../../../public/assets/tab-key.svg";
+import Image from "next/image";
 
 export default function Header() {
 	return (
@@ -8,6 +10,9 @@ export default function Header() {
 			<Link href={"/"}>
 				<div className={styles.text}>{metadata.blogName}</div>
 			</Link>
+			<div className={styles.tabKey}>
+
+			</div>
 		</div>
 	);
 }

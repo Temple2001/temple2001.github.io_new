fa1c3a081516b4356d3f7d23f521d0d4870dfd1b
동영 김 <sonic2001@nate.com>
2023-10-10 16:39:42 +0900

    Resolve Dark Mode Flickering Problem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,24 @@ import Layout from "@/components/layout";
 export default function RootLayout({ children }: Children) {
 	return (
 		<html lang="ko">
+			<head>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+					const saved = window.localStorage.getItem("data-theme");
+					if (saved) {
+						if (saved === "dark") {
+							document.documentElement.setAttribute("data-theme", "dark");
+						} else if (saved === "light") {
+							document.documentElement.setAttribute("data-theme", "light");
+						}
+					} else {
+						document.documentElement.setAttribute("data-theme", "light");
+					}
+				`,
+					}}
+				></script>
+			</head>
 			<body>
 				<Layout>{children}</Layout>
 			</body>
--- src/components/layout/dark-mode-button/index.tsx
+++ src/components/layout/dark-mode-button/index.tsx
@@ -1,23 +1,7 @@
-"use client";
-
 import styles from "./style.module.scss";
 import DarkModeIcon from "../../../../public/assets/darkmode.svg";
-import { useEffect } from "react";
 
 export default function DarkModeButton() {
-	useEffect(() => {
-		const saved = window.localStorage.getItem("data-theme");
-		if (saved) {
-			if (saved === "dark") {
-				document.documentElement.setAttribute("data-theme", "dark");
-			} else if (saved === "light") {
-				document.documentElement.setAttribute("data-theme", "light");
-			}
-		} else {
-			document.documentElement.setAttribute("data-theme", "light");
-		}
-	}, []);
-
 	const onChange = () => {
 		if (document.documentElement.getAttribute("data-theme") === "light") {
 			document.documentElement.setAttribute("data-theme", "dark");
